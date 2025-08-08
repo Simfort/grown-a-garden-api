d79fb5db3757c293d3e05f2b285f2c71c59f3495
Simfart <125682355+Simfort@users.noreply.github.com>
2025-08-08 11:08:26 +0300

    Delete api directory

--- api/dist/formating.d.ts
+++
@@ -1,6 +0,0 @@
-import { StockItem } from "./types.js";
-export default function formating(str: string): {
-    eggs: StockItem[];
-    seeds: StockItem[];
-    gears: StockItem[];
-};
--- api/dist/stock.d.ts
+++
@@ -1,6 +0,0 @@
-declare function checkForUpdates(): Promise<{
-    eggs: import("./types.js").StockItem[];
-    seeds: import("./types.js").StockItem[];
-    gears: import("./types.js").StockItem[];
-} | undefined>;
-export default checkForUpdates;
